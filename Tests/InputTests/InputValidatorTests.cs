0630bc7059519b01ab9bd565c5659117910bd514
irinaBalib <58845287+irinaBalib@users.noreply.github.com>
2021-08-30 17:32:56 +0300

    test project added

---
+++ Tests/InputTests/InputValidatorTests.cs
@@ -0,0 +1,22 @@
+using GameOfLife.Input;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class InputValidatorTests
+    {
+        private readonly InputValidator validator;
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+        }
+//Arrange
+        //Act
+        //Assert
+    }
+}
